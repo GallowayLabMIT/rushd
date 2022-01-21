fa7bc64bb3ca340c61c50fe098bc07833e9e8704
kaseylove <kslove@mit.edu>
2022-01-21 13:56:20 -0500

    wrote function load_with_metadata in flow.py added test cases to test_flow.py

---
+++ tests/test_flow.py
@@ -0,0 +1,11 @@
+import rushd
+from rushd.flow import load_with_metadata
+
+yaml_path = '/Users/kaseylove/Desktop/MIT/export_test/test.yaml'
+data_path = '/Users/kaseylove/Desktop/MIT/export_test/export'
+df = load_with_metadata(data_path,yaml_path)
+print(df)
+
+regex = r"^.*export_(?P<well>[A-G0-9]+)_(?P<population>.+)\.csv"
+df = load_with_metadata(data_path,yaml_path,filename_regex=regex)
+print(df)
